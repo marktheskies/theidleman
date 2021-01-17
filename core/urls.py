e81b024f3e371d3b5cfefce77f67cf0a895f57f0
Jessica Matterson-Jones <jess@Jessicas-MacBook-Pro.local>
2021-01-17 10:05:01 +1000

    Set up view, URL and template for single product details page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from core.views import products, home
+from core.views import products, home, product_details
 
 urlpatterns = [
     path('products', products, name='products'),
     path('', home, name='home'),
+    path('products/<int:id>', product_details, name='product_details'),
 ]
--- core/views.py
+++ core/views.py
@@ -10,6 +10,13 @@ def products(request):
     return render(request, "products.html", context)
 
 
+def product_details(request, id):
+    context = {
+        "product": Product.objects.get(id=id),
+    }
+    return render(request, "product_details.html", context)
+
+
 def home(request):
     first_three_products = Product.objects.all()[:3]
     context = {

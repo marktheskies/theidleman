54ed2ec18a7fd2a45de866d650ae1c56e3b5502f
markharris <marktheskies@gmail.com>
2021-01-21 12:53:13 +1100

    Misc. Black formatting

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class ProductCategory(models.Model):
     """A category for products, such as Hoodies, Trousers, etc."""
+
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
@@ -11,11 +12,14 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     """A product to sell"""
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
     discount_price = models.FloatField(null=True, blank=True)
-    category = models.ForeignKey(ProductCategory, models.SET_NULL, blank=True, null=True)
+    category = models.ForeignKey(
+        ProductCategory, models.SET_NULL, blank=True, null=True
+    )
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -24,8 +28,8 @@ class Product(models.Model):
 
 class ProductStock(models.Model):
     """A size and availability of a product"""
+
     size_code = models.CharField(max_length=255)
     size_name = models.CharField(max_length=255)
     quantity = models.IntegerField()
     product = models.ForeignKey(Product, models.CASCADE)
-
--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from core.views import products, home, product_details
 
 urlpatterns = [
-    path('products', products, name='products'),
-    path('', home, name='home'),
-    path('products/<int:id>', product_details, name='product_details'),
+    path("products", products, name="products"),
+    path("", home, name="home"),
+    path("products/<int:id>", product_details, name="product_details"),
 ]

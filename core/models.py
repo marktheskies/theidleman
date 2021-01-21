2b0cd981e06bdbc527e24cfcfc19054d902f04d9
markharris <marktheskies@gmail.com>
2021-01-21 13:28:44 +1100

    Add additional attributes to Product model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class ProductCategory(models.Model):
@@ -20,10 +21,29 @@ class Product(models.Model):
     category = models.ForeignKey(
         ProductCategory, models.SET_NULL, blank=True, null=True
     )
+
+    # The main image of the product
     image = models.ImageField(null=True, blank=True)
+
+    free_delivery = models.BooleanField(default=False)
+
+    # Rating is static, for now. TODO: Extend the rating to allow actual user reviews.
+    rating = models.IntegerField(
+        validators=[MaxValueValidator(5), MinValueValidator(0)],
+        default=0,
+    )
+
+    care_instructions = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class ProductAdditionalImage(models.Model):
+    """An additional image of a product"""
+
+    image = models.ImageField(null=True, blank=True)
+    product = models.ForeignKey(Product, models.CASCADE)
 
 
 class ProductStock(models.Model):
@@ -32,4 +52,6 @@ class ProductStock(models.Model):
     size_code = models.CharField(max_length=255)
     size_name = models.CharField(max_length=255)
     quantity = models.IntegerField()
+    color_name = models.CharField(max_length=255, default="Whisper")
+    color_hex = models.CharField(max_length=7, default="#EEEEEE")
     product = models.ForeignKey(Product, models.CASCADE)

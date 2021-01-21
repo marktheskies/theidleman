735725c085f5138d86760911c9c41626a6836f9b
markharris <marktheskies@gmail.com>
2021-01-21 16:47:35 +1100

    Dynamic product color selection

--- core/models.py
+++ core/models.py
@@ -21,7 +21,7 @@ class ProductCategory(models.Model):
 class Color(models.Model):
     """A color for a product"""
 
-    hex_value = models.CharField(max_length=7)
+    hex_value = models.CharField(max_length=7, unique=True)
 
     def __str__(self):
         return self.hex_value

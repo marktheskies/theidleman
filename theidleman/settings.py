11c4697d3af6cd2a868bfb34289e304e357d2fc6
markharris <marktheskies@gmail.com>
2021-02-12 10:20:23 +1100

    Sort imports

--- theidleman/settings.py
+++ theidleman/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import django_heroku
+import os
 from pathlib import Path
 
-import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
